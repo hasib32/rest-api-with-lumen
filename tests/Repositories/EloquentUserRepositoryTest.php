390e7e7c72e1711aff6c92a87f93bc0cb509083f
hasib32 <hasib32@gmail.com>
2017-03-21 14:43:01 -0400

    added repository test

---
+++ tests/Repositories/EloquentUserRepositoryTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Repositories;
+
+use App\Models\User;
+use App\Repositories\EloquentUserRepository;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class EloquentUserRepositoryTest extends \TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * @var EloquentUserRepository
+     */
+    protected $eloquentUserRepository;
+
+    public function setup()
+    {
+        parent::setUp();
+
+        $this->eloquentUserRepository = new EloquentUserRepository();
+    }
+
+    public function testCreateUser()
+    {
+        $testUserArray = factory(User::class)->make()->toArray();
+        $user = $this->eloquentUserRepository->save($testUserArray);
+
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertEquals($testUserArray['email'], $user->email);
+    }
+
+    public function testFindOne()
+    {
+        $testUser = factory(User::class)->create();
+
+        //first, check if it returns valid user
+        $user = $this->eloquentUserRepository->findOne($testUser->uid);
+        $this->assertInstanceOf(User::class, $user);
+
+        //now check it returns null for gibberish data
+        $user = $this->eloquentUserRepository->findOne('giberish');
+        $this->assertNull($user);
+    }
+
+    public function testFindOneBy()
+    {
+        $testUser = factory(User::class)->create();
+
+        //first, check if it returns valid user
+        $user = $this->eloquentUserRepository->findOneBy(['uid' => $testUser->uid]);
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertEquals($testUser->email, $user->email);
+
+        //check if it returns valid user, for multiple criteria
+        $user = $this->eloquentUserRepository->findOneBy([
+            'email'     => $testUser->email,
+            'firstName' => $testUser->firstName
+        ]);
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertEquals($testUser->firstName, $user->firstName);
+
+        //now check it returns null for gibberish data
+        $user = $this->eloquentUserRepository->findOneBy(['lastName' => 'Test Last']);
+        $this->assertNull($user);
+    }
+}
