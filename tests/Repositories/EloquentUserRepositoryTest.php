7421e769fd695ae068479345ce245b40354c957e
hasan hasibul <hasib32@gmail.com>
2017-03-22 18:55:56 -0400

    added endpoint tests

--- tests/Repositories/EloquentUserRepositoryTest.php
+++ tests/Repositories/EloquentUserRepositoryTest.php
@@ -14,11 +14,10 @@ class EloquentUserRepositoryTest extends \TestCase
      * @var EloquentUserRepository
      */
     protected $eloquentUserRepository;
-
+    
     public function setup()
     {
         parent::setUp();
-
         $this->eloquentUserRepository = new EloquentUserRepository();
     }
 
@@ -63,6 +62,53 @@ class EloquentUserRepositoryTest extends \TestCase
 
         //now check it returns null for gibberish data
         $user = $this->eloquentUserRepository->findOneBy(['lastName' => 'Test Last']);
+        $this->assertNull($user);
+    }
+
+    public function testFindBy()
+    {
+        //get total users of this resource
+        $totalUsers = User::all()->count();
+
+        //first, check if it returns all users without criteria
+        $users = $this->eloquentUserRepository->findBy([]);
+        $this->assertCount($totalUsers, $users);
+        //@todo need to add more tests
+    }
+
+    public function testUpdate()
+    {
+        $testUser = factory(User::class)->create([
+            'firstName' => 'test_first',
+            'lastName'  => 'test_last'
+        ]);
+
+        // First, test user instance
+        $user = $this->eloquentUserRepository->findOne($testUser->uid);
+        $this->assertInstanceOf(User::class, $user);
+
+        // Update user
+        $this->eloquentUserRepository->update($testUser, [
+            'firstName' => 'updated first_name',
+            'lastName'  => 'updated last_name'
+        ]);
+
+        // Fetch the user again
+        $user = $this->eloquentUserRepository->findOne($testUser->uid);
+        $this->assertEquals('updated first_name', $user->firstName);
+        $this->assertEquals('updated last_name', $user->lastName);
+        $this->assertNotEquals('test_first', $user->firstName);
+    }
+
+    public function testDelete()
+    {
+        $testUser = factory(User::class)->create();
+
+        $isDeleted = $this->eloquentUserRepository->delete($testUser);
+        $this->assertTrue($isDeleted);
+
+        // confirm deleted
+        $user = $this->eloquentUserRepository->findOne($testUser->uid);
         $this->assertNull($user);
     }
 }
