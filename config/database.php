c59d92a67ab80af4324ca3fa98a11f366207da1a
hasan hasibul <hasib32@gmail.com>
2017-03-22 13:43:55 -0400

    configured environment

--- config/database.php
+++ config/database.php
@@ -49,9 +49,9 @@ return [
         'testing' => [
             'driver'    => 'mysql',
             'host'      => env('DB_TEST_HOST', 'localhost'),
-            'database'  => env('DB_TEST_DATABASE', 'travis'),
+            'database'  => env('DB_TEST_DATABASE', 'restapi_test'),
             'username'  => env('DB_TEST_USERNAME', ''),
-            'password'  => env('DB_PASSWORD', ''),
+            'password'  => env('DB_TEST_PASSWORD', ''),
             'charset'   => env('DB_CHARSET', 'utf8'),
             'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
             'prefix'    => env('DB_PREFIX', ''),
